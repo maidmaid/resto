05f8f6dc0f00f77a7ad6924e00da0c8af574d63c
Thierry <crettolthierry@ringtarget.com>
2014-04-14 11:54:49 +0200

    Maj des entité Dish et Menu_dish en annotation
    Entity transformée de yml en annotation

---
+++ src/Mnu/MainBundle/Entity/Dish.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Mnu\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dish
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Mnu\MainBundle\Entity\DishRepository")
+ */
+class Dish
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="entitled", type="string", length=255)
+     */
+    private $entitled;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set entitled
+     *
+     * @param string $entitled
+     * @return Dish
+     */
+    public function setEntitled($entitled)
+    {
+        $this->entitled = $entitled;
+
+        return $this;
+    }
+
+    /**
+     * Get entitled
+     *
+     * @return string 
+     */
+    public function getEntitled()
+    {
+        return $this->entitled;
+    }
+}
