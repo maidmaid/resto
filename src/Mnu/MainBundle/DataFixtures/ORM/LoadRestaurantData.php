2c82dadd147c6b6046daa7aaae5d7f5ce7f9144b
Dany <danymaillard93b@gmail.com>
2014-04-14 12:23:24 +0200

    màj de la fixture Restaurant

--- src/Mnu/MainBundle/DataFixtures/ORM/LoadRestaurantData.php
+++ src/Mnu/MainBundle/DataFixtures/ORM/LoadRestaurantData.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mnu\UserBundle\DataFixtures\ORM;
+namespace Mnu\MainBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
