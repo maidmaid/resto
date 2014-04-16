851cd4db02b59121bdb4cab9e344abfa8af7b366
Vincent <vincent.huck.pro@gmail.com>
2014-04-16 20:42:48 +0200

    Création de l'entité Menu

---
+++ src/Mnu/MainBundle/Entity/Menu.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Mnu\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Menu
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Mnu\MainBundle\Entity\MenuRepository")
+ */
+class Menu
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="entitled", type="string", length=60)
+     */
+    private $entitled;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", scale=2)
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=45)
+     */
+    private $image;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set entitled
+     *
+     * @param string $entitled
+     * @return Menu
+     */
+    public function setEntitled($entitled)
+    {
+        $this->entitled = $entitled;
+
+        return $this;
+    }
+
+    /**
+     * Get entitled
+     *
+     * @return string 
+     */
+    public function getEntitled()
+    {
+        return $this->entitled;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Menu
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Menu
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
