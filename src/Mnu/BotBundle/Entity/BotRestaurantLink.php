f83ee7f8d63e4469c52857702c05233d931eb41b
Dany <danymaillard93b@gmail.com>
2014-05-12 12:15:23 +0200

    ajout des entités de liens de restaurant pour le bot

---
+++ src/Mnu/BotBundle/Entity/BotRestaurantLink.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Mnu\BotBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BotRestaurantLink
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Mnu\BotBundle\Entity\BotRestaurantLinkRepository")
+ */
+class BotRestaurantLink
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return BotRestaurantLink
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
