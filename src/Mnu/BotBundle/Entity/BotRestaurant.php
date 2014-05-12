6e1f7396ea897db1fe198e24e5358021bb828a4c
Dany <danymaillard93b@gmail.com>
2014-05-12 11:57:21 +0200

    ajout d'une entité BotRestaurant

---
+++ src/Mnu/BotBundle/Entity/BotRestaurant.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Mnu\BotBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BotRestaurant
+ *
+ * @ORM\Table(name="bot_restaurant")
+ * @ORM\Entity(repositoryClass="Mnu\BotBundle\Entity\BotRestaurantRepository")
+ */
+class BotRestaurant
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=10)
+     */
+    private $number;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param string $number
+     * @return BotRestaurant
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return string 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return BotRestaurant
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
